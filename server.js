ed06b6dd534da8675a442ff26407da63ee0ef84f
김성훈(Seonghun Kim)_인턴 <sh.kim14@konai.com>
2023-08-28 16:29:16 +0900

    feat: visit count 로직 작성

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const low = require('lowdb');
+const FileSync = require('lowdb/adapters/FileSync');
+
+const app = express();
+const adapter = new FileSync('db.json');
+const db = low(adapter);
+
+app.use(express.json());
+
+db.defaults({ visitors: 0 }).write();
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  // 접속자 수 증가
+  db.update('visitors', n => n + 1).write();
+  
+  const visitors = db.get('visitors').value();
+  
+  res.json({ message: 'Hello, welcome to the website!', visitors });
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
